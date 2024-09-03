be57ccbf00cbf3931a99479a25227898927ebacc
Teresa <tqwu@ucsc.edu>
2024-09-02 18:34:46 -0700

    Adding working game between two players

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,4 @@
 
-# from typing import List
 from helper import checkWin, printBoard, printHelper
 
 printHelper()
@@ -11,39 +10,27 @@ marked = set()
 # Continuously read user input until board is filled or winner is declared
 while True:
 
-    # Player 1's turn
-    if user_X:
-        move = int(input("\nPlayer 1, enter your square: "))
-        if not (1 <= move <= 9):
-            print('Invalid entry, please try again')
-            continue
-        if move in marked:
-            print("Tile has already been filled, please select another tile")
-            continue
-        board[move - 1] = 'X'
-        if checkWin(board):
-            print('\nPlayer 1 is winner!')
-            break
-        marked.add(move)
-        printBoard(board)
-        user_X = False
+    player = 1 if user_X else 2
 
-    # Player 2's turn
-    else:
-        move = int(input("\nPlayer 2, enter your square: "))
-        if not (1 <= move <= 9):
-            print('Invalid entry, please try again')
-            continue
-        if move in marked:
-            print("Tile has already been filled, please select another tile")
-            continue
-        board[move - 1] = 'O'
-        if checkWin(board):
-            print('\nPlayer 2 is winner!')
-            break
-        marked.add(move)
-        printBoard(board)
-        user_X = True
+    move = input(f"\nPlayer {player}, enter your selection: ")
+
+    if not move.isdigit() or not (1 <= int(move) <= 9):
+        print('Invalid entry, please try again')
+        continue
+
+    if move in marked:
+        print("Tile has already been filled, please select another tile")
+        continue
+
+    board[int(move) - 1] = 'X' if user_X else 'O'
+
+    if checkWin(board):
+        print(f"CONGRATULATIONS!!! Player {player} is winner!")
+        break
+
+    marked.add(move)
+    printBoard(board)
+    user_X = not user_X
 
     # If board is completely filled and no winner was declared, result is a draw
     if len(marked) == 9:
